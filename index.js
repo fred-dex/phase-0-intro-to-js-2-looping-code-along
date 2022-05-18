7910db4441418df0d5a11f2c1b14d6aaf3355441
freddex <fredjuiceman@yahoo.com>
2022-05-18 13:58:02 -0400

    commit tree a113867b17a74264614f0729138ef12d0efa8979

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards( namesArray, event) {
+    let writeCards = []
+    for (let i = 0; i < namesArray.length; i++) {
+        writeCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return writeCards
+}
+   
+function countDown(countdown) {
+    while (countdown > 0) {
+      console.log(countdown);
+      countdown -= 1;
+    }
+      console.log(countdown);
+ }
+
